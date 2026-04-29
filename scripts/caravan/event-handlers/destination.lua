f4bb6a05f17cac7c3130d36da14ddfb911775dec
AFranticTypist <billrenner@gmail.com>
2026-04-28 21:29:11 -0400

    Update destination.lua
    CaravanFuncs

--- scripts/caravan/event-handlers/destination.lua
+++ scripts/caravan/event-handlers/destination.lua
@@ -93,7 +93,7 @@ gui_events[defines.events.on_gui_click]["py_caravan_destination_play_stop_button
     local caravan_data = storage.caravans[tags.unit_number]
     local schedule = caravan_data.schedule[tags.schedule_id]
 
-	CaravanFuncs.wake_up( tags.unit_number ) 
+	CaravanImpl.wake_up( tags.unit_number ) 
 
     if caravan_data.schedule_id == tags.schedule_id then
         CaravanImpl.stop_actions(caravan_data)
